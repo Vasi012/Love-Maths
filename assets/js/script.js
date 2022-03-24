ff521197baa39e3e59e315f67dcbd827fa11fbce
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2022-03-24 23:05:33 +0000

    add +

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
                     alert("You clicked Submit!");
                 } else {
                     let gameType = this.getAttribute("data-type");
-                    alert(`You clicked ${gameType}`);
+                    runGame("gameType");
             }
         })
-}
+    }
+
+    runGame("addition");
 })
 
 
@@ -21,12 +23,20 @@ document.addEventListener("DOMContentLoaded", function () {
  * The main game "loop", called whent the script is first loaded
  * and after the user answer has been processed
 */
-function runGame() {
+function runGame(gameType) {
 
     // Create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1; 
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert (`Unknow game type: ${gameType}`);
+        throw `Unknow game type: ${gameType}. Aborting!`;
+    }
 }
+
 
 function checkAnswer() {
 
@@ -44,7 +54,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
