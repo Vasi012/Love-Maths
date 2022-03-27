03382c0762eba71ccc06878adf00601ccd9b3246
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2022-03-27 19:21:43 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You gotit right! :D");
+        incrementScore();
     } else {
         alert(`Awwwww....... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}! `);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -73,11 +75,24 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the curent score from the DOM and increments it by 1
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
